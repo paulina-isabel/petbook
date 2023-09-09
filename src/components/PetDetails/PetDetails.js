e91a1071cda07267eba46ebd10481efcd6cfa294
Judy Ye <judyy.ye@gmail.com>
2023-09-09 15:03:14 -0700

    Add styling to PetDetails

--- src/components/PetDetails/PetDetails.js
+++ src/components/PetDetails/PetDetails.js
@@ -5,18 +5,20 @@ function PetDetails({petOwnerInfo}) {
      return (
       <article key={petAndOwner.id} id={petAndOwner.id}>
         <button>Back to All Pets</button>
-        <div>
-          <img></img>
-          <p>Owner Name: {petAndOwner.ownerName} </p>
-          <p>Owner Details: </p>
-        </div>
-        <div>
-          <h2>Pet Name: {petAndOwner.petName}</h2>
-          <p> Type: {petAndOwner.type}</p>
-          <p> Type: {petAndOwner.breed}</p>
-          <p> Nicknames: </p>
-          <p> Fun Facts: </p>
-        </div>
+        <section className='pet-owner-info-container'>
+          <div className='owner-info'>
+            <span>🐱</span>
+            <p>Owner Name: {petAndOwner.ownerName} </p>
+            <p>Owner Details: </p>
+          </div>
+          <div className='pet-info'>
+            <h2>Pet Name: {petAndOwner.petName}</h2>
+            <p> Type: {petAndOwner.type}</p>
+            <p> Breed: {petAndOwner.breed}</p>
+            <p> Nicknames: </p>
+            <p> Fun Facts: </p>
+          </div>
+        </section>
       </article>
     )
   }
