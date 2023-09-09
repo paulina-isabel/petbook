9b007aa5039f7954e360f17115532e92d4e59d4f
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-09 17:15:12 -0400

    Render Form inside of HomeView.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import HomeView from './components/HomeView/HomeView';
-import Form from './components/Forms/Form';
+
 import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
-
-
 
 function App() {
   
   return (
     <div className="App">
       <Header />
-      <Form />
+
       <Routes>
         <Route path="/" element={<HomeView />}/>
       </Routes>
--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,8 +1,10 @@
-import AllPets from "../AllPets/AllPets"
+import AllPets from "../AllPets/AllPets";
+import Form from '../Forms/Form'
 
 const HomeView = () => {
   return (
     <div className="home-view">
+      <Form />
       <AllPets />
     </div>
   )
