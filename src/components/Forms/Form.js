614714103acf6b6a27e93f4c49b95138c358ba8b
Alice Abarca <aliceabarca@otmail.com>
2023-09-09 11:44:32 -0600

    Add use state for form inputs

---
+++ src/components/Forms/Form.js
@@ -0,0 +1,14 @@
+import './Form.css';
+import { useState } from 'react';
+
+function Form() {
+  const [petName, setPetName] = useState("");
+  const [petNickname, setPetNickname] = useState("");
+  const [petAge, setPetAge] = useState("");
+  const [petFunFact, setPetFunFact] = useState("");
+  const [petOwnersName, setPetOwnersName] = useState("");
+
+
+}
+
+export default Form;
