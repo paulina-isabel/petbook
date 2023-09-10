7706f042553e114980d932bc8751a5fd75a8f454
Judy Ye <judyy.ye@gmail.com>
2023-09-09 19:06:43 -0700

    Refactor PetDetails

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import './App.css';
 import Form from './components/Forms/Form';
 import Header from './components/Header/Header';
 import PetDetails from './components/PetDetails/PetDetails';
-import { useState } from 'react';
 
 
 
 function App() {
-  const petInfo = [
-    { id: '1', ownerName: 'Judy', petName: 'Oreo', type: 'cat', breed: 'domesticShorthair'},
-    { id: '2', ownerName: 'Alex', petName: 'Duke', type: 'dog', breed: 'schnoodle'},
-    { id: '3', ownerName: 'Paulina', petName: 'Pluto', type: 'cat', breed: 'Bombay'},
-    { id: '4', ownerName: 'Alice', petName: 'Rocco', type: 'dog', breed:'pitbull'},
-  ];
-
-  const [petOwnerInfo, setPetOwnerInfo] = useState(petInfo)
 
   return (
     <div className="App">
       <Header />
       <Form />
-      <PetDetails petOwnerInfo={petOwnerInfo} />
     </div>
   );
 }
--- src/components/PetDetails/PetDetails.js
+++ src/components/PetDetails/PetDetails.js
@@ -1,30 +1,29 @@
+import { useState } from 'react'
 import './PetDetails.css'
 
 function PetDetails({petOwnerInfo}) {
-  return petOwnerInfo.map(petAndOwner => {
-     return (
-      <article key={petAndOwner.id} id={petAndOwner.id}>
+  const [selectedPetAndOwner, setSeclectedPetAndOwner] = useState(petOwnerInfo)
+  console.log('ey', selectedPetAndOwner)
+  
+    return (
+      <article key={selectedPetAndOwner.id} id={selectedPetAndOwner.id}>
         <button>Back to All Pets</button>
         <section className='pet-owner-info-container'>
           <div className='owner-info'>
             <span>🐱</span>
-            <p>Owner Name: {petAndOwner.ownerName} </p>
+            <p>Owner Name: {selectedPetAndOwner.petOwnersName} </p>
             <p>Owner Details: </p>
           </div>
           <div className='pet-info'>
-            <h2>Pet Name: {petAndOwner.petName}</h2>
-            <p> Type: {petAndOwner.type}</p>
-            <p> Breed: {petAndOwner.breed}</p>
-            <p> Nicknames: </p>
-            <p> Fun Facts: </p>
+            <h2>Pet Name: {selectedPetAndOwner.petName}</h2>
+            <p>Nickname: {selectedPetAndOwner.petNickname}</p>
+            <p>Pet Age: {selectedPetAndOwner.petAge}</p>
+            <p>Fun Facts: {selectedPetAndOwner.petFunFact}</p>
           </div>
         </section>
       </article>
-    )
-  }
-
-   
-    )
+  )
 }
 
 export default PetDetails
+
