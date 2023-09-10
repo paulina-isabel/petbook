cc6266e874edd54f4af169dedf289013a7022171
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-10 13:53:52 -0400

    Add styling to pet cards.

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -1,10 +1,20 @@
+import './PetCard.css'
+import cat from '../../images/cat.png'
+import dog from '../../images/dog.png'
+
 const PetCard = ({ name, age, owner, funFact }) => {
+
+  const petIcon = () => {
+    
+  }
+
   return (
     <div className="pet-card">
+      <img className='pet-icon' src={cat} alt='pet icon'/>
       <h2>{name}</h2>
-      <p>{age} years old</p>
-      <p>Owner: {owner}</p>
-      <p>Fun Fact: {funFact}</p>
+      <p className='age'>{age} years old</p>
+      <p className='owner'>Owner: {owner}</p>
+      <p className='fun-fact'>Fun Fact: {funFact}</p>
     </div>
   )
 }
