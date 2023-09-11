53c7a00ca2600ab31a33e232260c36d0129d47d1
Judy Ye <judyy.ye@gmail.com>
2023-09-11 13:52:42 -0700

    Change spinner to spinning cat gif

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,11 +3,9 @@ import './Loading.css'
 const Loading = () => {
   return (
     <div className='spinner' role='status'>
-      <span className='sr-only'>LOADING</span>
+      <span className='sr-only'>Loading</span>
+      <img aria-hidden='true' alt="spinning cat" src="https://media.giphy.com/media/3o7TKtbdY5oZuiyucg/giphy.gif" />
     </div>
-
-
-    
   )
 }
 
