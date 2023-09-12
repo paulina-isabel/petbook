2d91e58b17fb9cd10b449af4776f14649594c6a0
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-12 01:12:10 -0400

    Render bone or fish for link depending on type.

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -1,36 +1,39 @@
 import './PetCard.css';
 import cat from '../../images/cat.png';
 import dog from '../../images/dog.png';
+import fish from '../../images/fish.png';
+import bone from '../../images/bone.png';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 const PetCard = ({ id, name, age, owner, funFact, nickname, type }) => {
 
-  const checkType = (type) => {
-    if (type === 'Dog') {
-      return <img src={dog} alt="Dog Image" />
-    } else {
-      return <img src={cat} alt="Cat Image" />
+  const checkIcon = (petType) => {
+    if (petType === 'Dog') {
+      return <img className='pet-icon' src={dog} alt="Dog Icon" />
+    } else if (petType === 'Cat'){
+      return <img className='pet-icon' src={cat} alt="Cat Icon" />
     }
   }
 
-  console.log(type)
+  const checkLinkImage = (petType) => {
+    if (petType === 'Dog') {
+      return <img className='detail-link-icon' src={bone} alt="Dog Detail Link" />
+    } else if (petType === 'Cat'){
+      return <img className='detail-link-icon' src={fish} alt="Cat Detail Link" />
+    }
+  }
 
   return (
     <div className="pet-card">
-        {type === 'Dog' ? (
-          <img className='pet-icon' src={dog} alt="Dog Image" />
-        ) : (
-          <img className='pet-icon' src={cat} alt="Cat Image" />
-        )}
-
+      {checkIcon(type)}
       <h2>{name}</h2>
       <h3>{type}</h3>
       <p className='nickname'>Nickname: {nickname}</p>
       <p className='age'>Age: {age} y/o</p>
       <p className='owner'>Owner: {owner}</p>
       <p className='fun-fact'>Fun Fact: {funFact}</p>
-      <Link to={`/${id}`}><img></img>Image here</Link>
+      <Link to={`/${id}`}>{checkLinkImage(type)}</Link>
     </div>
   )
 }
