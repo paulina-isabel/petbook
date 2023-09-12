d754b5acb4d1a77c9bbd90d015c573c54eeda509
Alice Abarca <aliceabarca@otmail.com>
2023-09-11 18:33:36 -0600

    Add cypress testing

---
+++ cypress/e2e/form.cy.js
@@ -0,0 +1,12 @@
+describe('test form', () => {
+  beforeEach(() => {
+  //   cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
+  //     statusCode: 200,
+  //     fixture: 'example.json'
+  //   })
+  //   .visit('http://localhost:3000')
+  // })
+  // it('passes', () => {
+  //   cy.visit('https://example.cypress.io')
+  })
+})
