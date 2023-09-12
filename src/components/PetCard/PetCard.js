63726c6471f7790015b0cd6e76d2e7357641a805
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-12 02:23:47 -0400

    Change use of useParams, parseInt pet id.

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -1,12 +1,8 @@
 import './PetCard.css';
-
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import { checkIcon, checkLinkImage } from '../../utils';
 
 const PetCard = ({ id, name, owner, type }) => {
-
-
 
   return (
     <div className="pet-card">
--- src/components/PetDetails/PetDetails.js
+++ src/components/PetDetails/PetDetails.js
@@ -3,40 +3,34 @@ import { Link, useParams } from 'react-router-dom'
 import cat from '../../images/cat.png'
 
 function PetDetails({ allPets }) {
-  const { id } = useParams()
+  const id = useParams().id;
 
   const foundPet = allPets.find((pet) => {
-      console.log(pet.id, 'pet.id in finder function')
-      console.log(id, 'id in finder function')
-      console.log(pet, 'this is pet in finder function')
-      const petResult = pet.id === id
-      console.log(petResult, 'this is petResult in finderFunction')
-      console.log(allPets, 'allPets')
+      const petResult = pet.id === parseInt(id)
       return petResult
     }
-  )
+  );
 
-    return foundPet && (
-      <article key={foundPet.id}>
-        <div className='back-to-all-pets-button-container'>
-          <Link to={'/'}><button className='back-to-all-pets-button'>Back to All Pets</button></Link>
+  return foundPet && (
+    <article key={foundPet.id}>
+      <div className='back-to-all-pets-button-container'>
+        <Link to={'/'}><button className='back-to-all-pets-button'>Back to All Pets</button></Link>
+      </div>
+      <section className='details-container'>
+        <div className='owner-info'>
+          <img className='pet-image'src={cat} alt='cat' />
+          <h2>{foundPet.petName}</h2>
+          <p>Owner: {foundPet.petOwnersName} </p>
         </div>
-        <section className='details-container'>
-          <div className='owner-info'>
-            <img className='pet-image'src={cat} alt='cat' />
-            <h2>{foundPet.petName}</h2>
-            <p>Owner: {foundPet.petOwnersName} </p>
-          </div>
-          <div className='pet-info'>
-            <h3>{foundPet.type}</h3>
-            <p>Nickname: {foundPet.petNickname}</p>
-            <p>Age: {foundPet.petAge}</p>
-            <p>Fun Facts: {foundPet.petFunFact}</p>
-          </div>
-        </section>
-      </article>
-  )
-}
+        <div className='pet-info'>
+          <h3>{foundPet.type}</h3>
+          <p>Nickname: {foundPet.petNickname}</p>
+          <p>Age: {foundPet.petAge}</p>
+          <p>Fun Facts: {foundPet.petFunFact}</p>
+        </div>
+      </section>
+    </article>
+  );
+};
 
-export default PetDetails
-
+export default PetDetails;
