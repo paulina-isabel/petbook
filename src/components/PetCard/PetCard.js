7eb3470662417b5fb3624196c61341b17c2a7488
Alex Peterson <aspeterson.126@gmail.com>
2023-09-12 22:42:27 -0500

    Add delete icon to PetCard components

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -1,11 +1,15 @@
 import './PetCard.css';
 import { Link } from 'react-router-dom';
 import { checkIcon, checkLinkImage } from '../../utils';
+import deleteIcon from '../../images/delete.png';
 
 const PetCard = ({ id, name, owner, type }) => {
 
   return (
     <div className="pet-card">
+      <div className='icon-container'>
+        <img className='delete-icon' src={deleteIcon} alt='delete icon'/>
+      </div>
       {checkIcon(type)}
       <h2>{name}</h2>
       <h3>{type}</h3>
