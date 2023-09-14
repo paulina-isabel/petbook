4b9ba99cb9a7b47ef78c14cdcdec66e24d95c32e
Judy Ye <judyy.ye@gmail.com>
2023-09-13 22:54:46 -0700

    Add Link back to HomeView on Error page

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,12 +1,13 @@
 import './Error.css';
 import shockedCat from '../../images/hang-niu-Tn8DLxwuDMA-unsplash.jpg';
+import { Link } from 'react-router-dom';
 
 const Error = ({error}) => {
   return (
     <section className='error'>
       <h2>{error}</h2>
       <img src={shockedCat}/>
-      <button className='error-button'>Back to Home</button>
+      <Link to={'/'}><button className='error-button'>Back to Home</button></Link>
     </section>
   )
 }
