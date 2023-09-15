6ca38c4d56bba89231f1edf9857a5a8fdc932284
Alice Abarca <aliceabarca@otmail.com>
2023-09-14 19:47:25 -0600

    Add test for pets name and owners name

--- cypress/e2e/home-view.cy.js
+++ cypress/e2e/home-view.cy.js
@@ -10,14 +10,15 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000')
     cy.get('.petbook-logo')
       .should('have.attr', 'alt')
-    // cy.get('h1')
-    //   .should()
+    cy.get('h1')
+      .should('contain', 'PetB🐾k')
     cy.get('.title-container > h2')
       .contains('Your Pet\'s Perfect Page')
   })
   it('should delete pet card', () => {
     cy.get('.delete-icon')
       .should('have.attr', 'alt')
+      // .click('img')
   })
   it('should display pets name and owners name', () => {
     cy.get(':nth-child(1) > .info-container > h2')
