564aad70cadc1b42f9fc611abff5eb01fd50a8f3
Alex Peterson <aspeterson.126@gmail.com>
2023-09-16 12:02:17 -0500

    Edit cypress pet details to stub single id api fetch

--- cypress/e2e/pet-details-spec.cy.js
+++ cypress/e2e/pet-details-spec.cy.js
@@ -1,31 +1,41 @@
-describe('template spec', () => {
+describe('Should test single pet details page', () => {
+  
   beforeEach(() => {
-    cy.intercept("GET", 'http://localhost:3001/api/v1/pets', {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
       statusCode: 200,
-      fixture: "duke.json"
+      fixture: "example.json"
     })
     cy.visit('http://localhost:3000/')
   })
 
   it('Should select pet and be shown pet details for that pet', () => {
-    cy.url().should('eq', 'http://localhost:3000/')
-    cy.get('.pet-card').get('.detail-link-icon').click()
-    cy.url().should('eq', 'http://localhost:3000/2')
-    cy.get('.details-container').get('.pet')
-      .get('.details').contains('Owner: Alex')
-      .get('.pet').find('.pet-icon[alt="Dog Icon"]')
+    cy.intercept('GET', `http://localhost:3001/api/v1/pets/1`,{
+      statusCode: 200,
+      fixture: 'singlePet.json'
+    }).as('check')
 
+    cy.get('.all-pets-container > :nth-child(1)')
+    cy.get(':nth-child(1) > .info-container')
+    cy.get(':nth-child(1) > .info-container > a > .detail-link-icon').click()
+   
+    cy.url().should('eq', 'http://localhost:3000/1')
+    cy.wait('@check')
     cy.get('.details-container')
-      .get('.pet').contains('Duke')
-      .get('.details').contains('Nickname: Doodle-bee')
-      .get('.details').contains('Age: 19')
-      .get('.details').contains('Fun Facts: He likes to go for car rides')
+    cy.get('.pet').get('.pet-image')
+    cy.get('.pet > h2').contains('Oreo') 
+    cy.get('.pet > p').contains('Owner: Judy')
+    cy.get('.pet-info')
+    cy.get('.pet-info > :nth-child(2)').contains('Nickname: Oreo')
+    cy.get('.pet-info > :nth-child(3)').contains('2.5')
+    cy.get('.pet-info > :nth-child(4)').contains('Fun Facts: hes had 2 types of tape worms')
+       
   })
 
   it('Should return home to all pets on button click', () => {
     cy.visit('http://localhost:3000/2')
     cy.url().should('eq', 'http://localhost:3000/2')
-    cy.get('.back-to-all-pets-button').click()
+    cy.get('.back-to-all-pets-button')
+    .click()
     cy.url().should('eq', 'http://localhost:3000/')
   })
 })
--- src/components/PetDetails/PetDetails.js
+++ src/components/PetDetails/PetDetails.js
@@ -1,7 +1,7 @@
 import './PetDetails.css'
 import spirals from '../../images/spirals.png'
 import { Link, useParams } from 'react-router-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import { checkIcon } from '../../utils';
 import { fetchPetsById } from '../../api-calls';
 import { useEffect, useState } from 'react';
@@ -22,19 +22,22 @@ function PetDetails({ setError}) {
     fetchPetsById(id)
     .then(data => {
       setFoundPet(data)
+      
     })
     .catch(err => setError(`${err.message}`))
   }, [])
   
+  console.log(foundPet)
   return Object.values(foundPet).length > 0 && (
+  // return (
     <article key={foundPet.id}>
       <div className='back-to-all-pets-button-container'>
         <Link to={'/'}><button className='back-to-all-pets-button'>Back to All Pets</button></Link>
       </div>
       <section className='details-container'>
-      <div className='spirals-container'>
-        <img className='spirals' src={spirals} alt='spiral styling'/>
-      </div>
+        <div className='spirals-container'>
+          <img className='spirals' src={spirals} alt='spiral styling'/>
+        </div>
         <div className='pet'>
           <div className='pet-image'>{checkIcon(foundPet.type)}</div>
           <h2>{foundPet.name}</h2>
@@ -53,15 +56,15 @@ function PetDetails({ setError}) {
 
 export default PetDetails;
 
-PetDetails.propTypes = {
-  allPets: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-      petName: PropTypes.string.isRequired,
-      petNickname: PropTypes.string,
-      petAge: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-      petFunFact: PropTypes.string,
-      petOwnersName: PropTypes.string.isRequired
-    })
-  )
-}
+// PetDetails.propTypes = {
+  // allPets: PropTypes.arrayOf(
+    // PropTypes.shape({
+      // id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      // petName: PropTypes.string.isRequired,
+      // petNickname: PropTypes.string,
+      // petAge: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      // petFunFact: PropTypes.string,
+      // petOwnersName: PropTypes.string.isRequired
+    // })
+  // )
+// }
