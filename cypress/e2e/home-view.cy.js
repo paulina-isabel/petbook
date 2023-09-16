288af850f114b00234e71849f1d5a01a2c4a7443
Alex Peterson <aspeterson.126@gmail.com>
2023-09-16 18:20:06 -0500

    Revert testing

--- cypress/e2e/home-view.cy.js
+++ cypress/e2e/home-view.cy.js
@@ -1,32 +1,34 @@
 describe('Test HomeView', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://petbook-be-git-refactor-edit-server-name-aliceabarca.vercel.app/api/v1/pets', {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
       statusCode: 200,
       fixture: 'allPets.json'
     })
-    .visit('https://petbook-5fjgm51ku-paulina-isabel.vercel.app/')
-  })
+    .visit('http://localhost:3000')
+  });
 
   it('should display header', () => {
-    cy.visit('https://petbook-5fjgm51ku-paulina-isabel.vercel.app/')
+    cy.visit('http://localhost:3000')
     cy.get('.petbook-logo')
       .should('have.attr', 'alt')
     cy.get('h1')
       .should('contain', 'PetB🐾k')
     cy.get('.title-container > h2')
       .contains('Your Pet\'s Perfect Page')
-  })
+  });
 
-  // it('should delete pet card', () => {
-    // cy.get('.delete-icon')
-      // .should('have.attr', 'alt')
-  // })
+  it('should delete pet card', () => {
+    cy.intercept('DELETE', 'http://localhost:3001/api/v1/pets/3', {
+      statusCode: 204
+    }).as('deleteRequest');
+    cy.get('.all-pets-container').find('.pet-card').should('have.length', 4)
+    cy.get('.all-pets-container > :nth-child(3)')
+      .get(':nth-child(3) > .info-container > .icon-container > .delete-icon').click();
+    cy.wait('@deleteRequest');
+    cy.get('.all-pets-container').find('.pet-card').should('have.length', 3);
+  });
 
   it('should display card', () => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
-      statusCode: 200,
-      fixture: 'allPets.json'
-    })
     cy.get(':nth-child(1) > .info-container > .pet-icon')
       .should('have.attr', 'alt')
     cy.get(':nth-child(1) > .info-container > h2')
@@ -35,47 +37,27 @@ describe('Test HomeView', () => {
       .should('contain', 'Cat')
     cy.get(':nth-child(1) > .info-container > .owner')
       .should('contain', 'Owner: Judy')
-  })
+  });
 
   it('should take you to pet details when link is clicked', () => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
-      statusCode: 200,
-      fixture: 'allPets.json'
-    })
-
     cy.get(':nth-child(1) > .info-container > a > .detail-link-icon').first().click()
-      .url().should('eq', 'https://petbook-5fjgm51ku-paulina-isabel.vercel.app/1')
-  })
+      .url().should('eq', 'http://localhost:3000/1')
+  });
   
   it('Should display error message with a 500 level error', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
-      statusCode: 500,
-      fixture: 'allPets.json'
-    })
-    cy.intercept('GET', 'https://petbook-be-git-refactor-edit-server-name-aliceabarca.vercel.app/api/v1/pets', {
       statusCode: 500})
-      cy.get('.error > h2').contains('Request failed - Unable to retrieve contacts from server.')    
-  })
+    cy.get('.error > h2').contains('Request failed - Unable to retrieve contacts from server.')    
+  });
 
-  // it('Should display URL error page with a 404 level error', () => {
-    // cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
-      // statusCode: 404,
-      // fixture: 'allPets.json'
-    // })
-// 
-    // cy.intercept('GET', 'https://petbook-5fjgm51ku-paulina-isabel.vercel.app/nonsense', {
-      // statusCode: 404,
-      // fixture: 'allPets.json'
-    // })
-// 
-    // cy.intercept('GET', 'https://petbook-be-git-refactor-edit-server-name-aliceabarca.vercel.app/api/v1/pets/nonsense', {
-      // statusCode: 404})
-      // 
-    //  cy.visit('https://petbook-5fjgm51ku-paulina-isabel.vercel.app/nonsense')
-    //  cy.get('.error > h2').contains('Unable to retrieve contacts from server.')    
-    //  cy.get('.error-button').click()
-    //  cy.url().should('eq', 'https://petbook-5fjgm51ku-paulina-isabel.vercel.app/')
-// })
-
-})
-  
+  it('Should display URL error page with a 404 level error', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/pets/nonsense', {
+      statusCode: 404
+    })  
+    cy.visit('http://localhost:3000/nonsense')
+    cy.url().should('eq', 'http://localhost:3000/nonsense')
+    cy.get('.error > h2').contains('Unable to retrieve contacts from server.')    
+    cy.get('.error-button').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
+});
--- cypress/e2e/pet-details-spec.cy.js
+++ cypress/e2e/pet-details-spec.cy.js
@@ -1,39 +1,22 @@
 describe('Should test single pet details page', () => {
   
   beforeEach(() => {
-    cy.intercept('GET', 'https://petbook-be-git-refactor-edit-server-name-aliceabarca.vercel.app/api/v1/pets', {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
       statusCode: 200,
       fixture: "allPets.json"
-    }).visit('https://petbook-5fjgm51ku-paulina-isabel.vercel.app/pets')
-  })
+    })
+    cy.visit('http://localhost:3000/')
+  });
 
   it('Should select pet and be shown pet details for that pet', () => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/pets/', {
-      statusCode: 200,
-      fixture: 'allPets.json'
-    })
-   
-
-    // cy.intercept('GET', 'http://localhost:3001/api/v1/pets/1', {
-      // statusCode: 200,
-      // fixture: 'allPets.json'
-    // })
-
-    cy.intercept('GET', `https://petbook-be-git-refactor-edit-server-name-aliceabarca.vercel.app/api/v1/pets/1`,{
+    cy.intercept('GET', `http://localhost:3001/api/v1/pets/1`,{
       statusCode: 200,
       fixture: 'singlePet.json'
     }).as('check')
-
     cy.get('.all-pets-container > :nth-child(1)')
     cy.get(':nth-child(1) > .info-container')
     cy.get(':nth-child(1) > .info-container > a > .detail-link-icon').click()
-
- 
- 
- 
- 
- 
-    cy.url().should('eq', 'https://petbook-5fjgm51ku-paulina-isabel.vercel.app/1')
+    cy.url().should('eq', 'http://localhost:3000/1')
     cy.wait('@check')
     cy.get('.details-container')
     cy.get('.pet').get('.pet-image')
@@ -42,38 +25,34 @@ describe('Should test single pet details page', () => {
     cy.get('.pet-info')
     cy.get('.pet-info > :nth-child(2)').contains('Nickname: Oreo')
     cy.get('.pet-info > :nth-child(3)').contains('2.5')
-    cy.get('.pet-info > :nth-child(4)').contains('Fun Facts: He\'s had 2 types of tape worms')
-       
-  })
+    cy.get('.pet-info > :nth-child(4)').contains('Fun Facts: He\'s had 2 types of tape worms')    
+  });
 
   it('Should return home to all pets on button click', () => {
-    cy.intercept('GET', `https://petbook-5fjgm51ku-paulina-isabel.vercel.app/1`,{
+    cy.intercept('GET', `http://localhost:3001/api/v1/pets/1`,{
       statusCode: 200,
       fixture: 'singlePet.json'
     }).as('check')
-
-    cy.visit('https://petbook-5fjgm51ku-paulina-isabel.vercel.app/1')
-    cy.url().should('eq', 'https://petbook-5fjgm51ku-paulina-isabel.vercel.app/1')
+    cy.visit('http://localhost:3000/1')
+    cy.url().should('eq', 'http://localhost:3000/1')
     cy.wait('@check')
-    cy.get('.back-to-all-pets-button')
-    .click()
-    cy.url().should('eq', 'https://petbook-5fjgm51ku-paulina-isabel.vercel.app')
-  })
+    cy.get('.back-to-all-pets-button').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
 
-  // it('Should display error message with a 500 level error', () => {
-    // cy.intercept('GET', 'https://petbook-5fjgm51ku-paulina-isabel.vercel.app/', {
-      // statusCode: 500})
-      // cy.get('.error > h2').contains('Request failed - Unable to retrieve contacts from server.')    
-  // })
+  it('Should display error message with a 500 level error', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
+      statusCode: 500})
+    cy.get('.error > h2').contains('Request failed - Unable to retrieve contacts from server.')    
+  });
 
-//  it('Should display URL error page with a 404 level error', () => {
-  // cy.intercept('GET', 'http://localhost:3001/api/v1/pets/nonsense', {
-    // statusCode: 404})
-    // 
-  //  cy.visit('http://localhost:3000/nonsense')
-  //  cy.url().should('eq', 'http://localhost:3000/nonsense')
-  //  cy.get('.error > h2').contains('Unable to retrieve contacts from server.')    
-  //  cy.get('.error-button').click()
-  //  cy.url().should('eq', 'http://localhost:3000/')
-  //  })
-})
+ it('Should display URL error page with a 404 level error', () => {
+  cy.intercept('GET', 'http://localhost:3001/api/v1/pets/nonsense', {
+    statusCode: 404})
+  cy.visit('http://localhost:3000/nonsense')
+  cy.url().should('eq', 'http://localhost:3000/nonsense')
+  cy.get('.error > h2').contains('Unable to retrieve contacts from server.')    
+  cy.get('.error-button').click()
+  cy.url().should('eq', 'http://localhost:3000/')
+  });
+});
