83e29d3fc3047f711790b9a13abd2502f53e4833
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-16 13:21:13 -0700

    Organize code in all tests.

--- cypress/e2e/home-view.cy.js
+++ cypress/e2e/home-view.cy.js
@@ -5,7 +5,7 @@ describe('Test HomeView', () => {
       fixture: 'allPets.json'
     })
     .visit('http://localhost:3000')
-  })
+  });
 
   it('should display header', () => {
     cy.visit('http://localhost:3000')
@@ -15,9 +15,9 @@ describe('Test HomeView', () => {
       .should('contain', 'PetB🐾k')
     cy.get('.title-container > h2')
       .contains('Your Pet\'s Perfect Page')
-  })
+  });
 
-  it.only('should delete pet card', () => {
+  it('should delete pet card', () => {
     cy.intercept('DELETE', 'http://localhost:3001/api/v1/pets/3', {
       statusCode: 204
     }).as('deleteRequest');
@@ -28,7 +28,6 @@ describe('Test HomeView', () => {
     cy.get('.all-pets-container').find('.pet-card').should('have.length', 3);
   });
 
-
   it('should display card', () => {
     cy.get(':nth-child(1) > .info-container > .pet-icon')
       .should('have.attr', 'alt')
@@ -38,18 +37,18 @@ describe('Test HomeView', () => {
       .should('contain', 'Cat')
     cy.get(':nth-child(1) > .info-container > .owner')
       .should('contain', 'Owner: Judy')
-  })
+  });
 
   it('should take you to pet details when link is clicked', () => {
     cy.get(':nth-child(1) > .info-container > a > .detail-link-icon').first().click()
       .url().should('eq', 'http://localhost:3000/1')
-  })
+  });
   
   it('Should display error message with a 500 level error', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
       statusCode: 500})
-      cy.get('.error > h2').contains('Request failed - Unable to retrieve contacts from server.')    
-  })
+    cy.get('.error > h2').contains('Request failed - Unable to retrieve contacts from server.')    
+  });
 
   it('Should display URL error page with a 404 level error', () => {
     cy.visit('http://localhost:3000/nonsense')
@@ -57,5 +56,5 @@ describe('Test HomeView', () => {
     cy.get('.error > h2').contains('Unable to retrieve contacts from server.')    
     cy.get('.error-button').click()
     cy.url().should('eq', 'http://localhost:3000/')
-    })
-})
+  });
+});
