e4c19983eed0a9cc8667df5fb94d8faa0f8c633d
Alex Peterson <aspeterson.126@gmail.com>
2023-09-16 12:28:55 -0500

    Add error handling to pet details testing

--- cypress/e2e/pet-details-spec.cy.js
+++ cypress/e2e/pet-details-spec.cy.js
@@ -38,4 +38,18 @@ describe('Should test single pet details page', () => {
     .click()
     cy.url().should('eq', 'http://localhost:3000/')
   })
+
+  it('Should display error message with a 500 level error', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/pets', {
+      statusCode: 500})
+      cy.get('.error > h2').contains('Request failed - Unable to retrieve contacts from server.')    
+  })
+
+    it('Should display URL error page with a 404 level error', () => {
+      cy.visit('http://localhost:3000/nonsense')
+      cy.url().should('eq', 'http://localhost:3000/nonsense')
+      cy.get('.error > h2').contains('Unable to retrieve contacts from server.')    
+      cy.get('.error-button').click()
+      cy.url().should('eq', 'http://localhost:3000/')
+      })
 })
